cf36e81455a9860c71dfdeee0bd9dae0dc0da903
Danny Santos <danny@ethicalswitch.com>
2016-08-31 15:33:49 +0100

    A user resgisters

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,12 @@
+Given(/^they are on the registration page$/) do
+  visit root_path
+  click_on "Register"
+end
+
+When(/^they fill in the registration form with valid details$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^a User should be created$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end

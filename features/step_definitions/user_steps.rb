c9d00fa305e2bd395e622bced7b625e5d0ea5ea2
Danny Santos <danny@ethicalswitch.com>
2016-09-02 11:28:50 +0100

    A user logs out

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -16,6 +16,19 @@ Given(/^they are on the log in page$/) do
   visit new_user_session_path
 end
 
+Given(/^they have signed in previously$/) do
+  step "they are on the log in page"
+  step "they fill out the log in form with valid details"
+end
+
+Given(/^they are on the home page$/) do
+  visit root_path
+end
+
+When(/^they sign out$/) do
+  click_on "Sign out"
+end
+
 When(/^they fill out the log in form with valid details$/) do
   fill_in "Email", with: "test@test.com"
   fill_in "Password", with: "password"
@@ -35,4 +48,8 @@ end
 
 Then(/^they are logged in$/) do
   expect(page).to have_content("Signed in successfully.")
+end
+
+Then(/^they should see a farewell message$/) do
+  "Signed out successfully."
 end
--- features/user.feature
+++ features/user.feature
@@ -10,3 +10,10 @@ Feature: User
       And they are on the log in page
     When they fill out the log in form with valid details
     Then they are logged in
+
+  Scenario: A User signs out
+    Given a User has previously registered
+      And they have signed in previously
+      And they are on the home page
+    When they sign out
+    Then they should see a farewell message
